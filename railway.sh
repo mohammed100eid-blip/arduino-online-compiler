a5f43442994b2ace614b5f08cb1938ca549f488a
mohammed <mohammed100eid@gmail.com>
2026-04-20 12:33:35 +0200

    Update railway.sh

--- railway.sh
+++ railway.sh
@@ -4,13 +4,11 @@ echo "Installing Arduino CLI..."
 
 mkdir -p arduino-cli-bin
 
-curl -fsSL https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_64bit.tar.gz -o cli.tar.gz
+curl -L https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_64bit.tar.gz -o cli.tar.gz
 
 tar -xvf cli.tar.gz -C arduino-cli-bin
 
 chmod +x arduino-cli-bin/arduino-cli
-
-export PATH=$PATH:$(pwd)/arduino-cli-bin
 
 echo "Installing cores..."
 
